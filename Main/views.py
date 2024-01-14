3a1d88fc1868c4e0b63f51c94be021c18d5815bb
OdilbekDeveloper <desmon.developer@gmail.com>
2024-01-14 21:04:05 +0500

    v0.1

--- Main/views.py
+++ Main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .models import *
 # Create your views here.
 import requests
+from django.http import HttpResponse
 
 def Index(request):
     slider = Slider.objects.all()
@@ -36,15 +37,28 @@ def About(request):
 
 def Service(request):
     bg = Background_Img.objects.last()
-
+    img = Picture.objects.all()
+    info = Info.objects.last()
+    stats = Statistics.objects.last()
     context = {
-        'bg': bg
+        'bg': bg,
+        'img': img,
+        'stats': stats,
+        'info': info,
     }
     return render(request, 'service.html', context)
 
 def Project(request):
-    
-    return render(request, 'project-3-col.html')
+    product = Product.objects.all().order_by('-id')
+    bg = Background_Img.objects.last()
+    info = Info.objects.last()
+
+    context = {
+        'bg': bg,
+        'product': product,
+        'info': info,
+    }
+    return render(request, 'project-3-col.html', context)
 
 def Project_Detail(request):
     
@@ -54,13 +68,26 @@ def Blogs(request):
     return render(request, 'blog-sidebar-right.html')
 
 def BlogsFull(request):
+    product = Product.objects.all().order_by('-id')[:6]
+    info = Info.objects.last()
+    bg = Background_Img.objects.last()
+
+    context = {
+        'product': product,
+        'bg': bg,
+        'info':info
+    }
     
-    return render(request, 'blog-full-width.html')
+    
+    return render(request, 'blog-full-width.html', context)
+
+# def Contact(request):
+    
+#     return render(request, 'contact.html')
 
 def Contact(request):
     
-    return render(request, 'contact.html')
-
+    return HttpResponse('Эта страница в настоящее время находится в разработке')
 
 def Project_Details(request):
     
