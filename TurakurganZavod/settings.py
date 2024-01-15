6ad64fd95397fe1ec7aaf50ff388bae0e892c0b1
OdilbekDeveloper <desmon.developer@gmail.com>
2024-01-15 12:04:53 +0500

    pull blog

--- TurakurganZavod/settings.py
+++ TurakurganZavod/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egnjw=0fjzvko!%q#759xinwl-am9c@hq=(cq-75njtc9dcifu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
